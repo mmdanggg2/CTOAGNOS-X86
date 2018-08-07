e4a7a3bb971b62997d632c17c4bcb0850a843965
James Horsley <jbhorsley@gmail.com>
2018-08-07 19:59:11 +0100

    Added quick setup script for installing gcc-7

---
+++ enviroSetup.sh
@@ -0,0 +1,11 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt update
+sudo apt install g++-7 -y
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 \
+                         --slave /usr/bin/g++ g++ /usr/bin/g++-7 
+sudo update-alternatives --config gcc
+
+echo 
+echo Also do \"sudo visudo\" and set to NOPASSWD
+echo
