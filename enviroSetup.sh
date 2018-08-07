f952c46d4f53ad4026db3b7bdd60cf65401c87ff
James Horsley <jbhorsley@gmail.com>
2018-08-07 20:35:15 +0100

    Added more pre-reqs to setup

--- enviroSetup.sh
+++ enviroSetup.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update
-sudo apt install g++-7 -y
+sudo apt install g++-7 nasm mtools gcc-7-multilib g++-7-multilib -y
 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 \
                          --slave /usr/bin/g++ g++ /usr/bin/g++-7 
