f158fafeaffc5d62a8861499f354ea14238bf317
James Horsley <jbhorsley@gmail.com>
2018-08-14 00:16:30 +0100

    Console typing now works with backspace and proper termination at the end of the line.

--- source/system/Console.cpp
+++ source/system/Console.cpp
@@ -11,19 +11,22 @@ void Console::drawCmdLine() {
 	video.drawString(">"); video.drawCur();
 }
 
-void advanceCur() {
+void Console::advanceCur(bool toNextLine) {
+	if (video.curX >= video.getCols() - 1) {
+		if (toNextLine) {
+			advanceLine();
+		}
+		return;
+	}
 	video.curX++;
 }
 
-void handleChar(uint8_t chr) {
-	video.drawChar(chr);
-	advanceCur();
-	video.drawCur();
-	if (video.curX >= video.getCols() - 1) {
-		DisplayColor preCol = video.cursorCol;
-		video.cursorCol = DisplayColor(0xC).setBlink();
-		video.drawCur();
-		video.cursorCol = preCol;
+void Console::advanceLine() {
+	video.curX = startX;
+	video.curY++;
+	if (video.curY >= video.getRows()) {
+		video.curY = video.getRows() - 1;
+		video.scrollUp();
 	}
 }
 
@@ -46,6 +49,7 @@ int Console::run() {
 		case KB_ESCAPE:
 			break;
 		case KB_BACKSPACE:
+			handleBackspace();
 			break;
 		case KB_RETURN:
 			break;
@@ -62,4 +66,32 @@ int Console::run() {
 
 	}
 	return 8;
+}
+
+void Console::handleChar(uint8_t chr) {
+	DisplayColor preCol = video.cursorCol;
+	video.drawChar(chr);
+	advanceCur();
+	if (video.curX >= video.getCols() - 1) {
+		video.cursorCol = DisplayColor(0xC).setBlink();
+	}
+	if (cmdPos < CON_CMD_LEN) {
+		cmdBuffer[cmdPos++] = chr;
+	}
+
+	video.drawCur();
+	video.cursorCol = preCol;
+}
+
+void Console::handleBackspace() {
+	if (video.curX <= startX) { 
+		return;
+	}
+	video.drawChar(0x20);// draw blank space
+	video.curX--;
+	video.drawCur();
+	if (cmdPos > 0) {
+		cmdPos--;
+	}
+	cmdBuffer[cmdPos] = 0;// null the end of the command
 }
--- source/system/Console.h
+++ source/system/Console.h
@@ -3,12 +3,20 @@
 class Console
 {
 	uint8_t keyInp = 0;
+#define CON_CMD_LEN 78
+	uint8_t cmdBuffer[CON_CMD_LEN];//TODO custom command length?
+	uint8_t cmdPos = 0;
 
-	uint8_t cmdBuffer[78];//TODO custom command length?
+	int startX = 1;
 
 	void drawCmdLine();
+	void handleChar(uint8_t chr);
+	void handleBackspace();
 public:
 	Console();
+
+	void advanceCur(bool toNextLine = false);
+	void advanceLine();
 
 	int run();
 };
